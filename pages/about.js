2fab61d8b1e6a757211a9705ddb3ccf6cf4aa3bc
Chris Ellis <ellis.cs@gmail.com>
2021-03-12 10:16:49 +0000

    feat: add resume

--- pages/about.js
+++ pages/about.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Resume</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">About</a>
         </h1>
 
         <p className={styles.description}>
@@ -19,11 +19,7 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <p className="">
-          <a href="/DemoResume.pdf">Resume</a>
-        </p>
-
-        
+      
       </main>
 
       <footer className={styles.footer}>
